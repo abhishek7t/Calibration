5644f242fed247ca9baa4de52028c0895f9572bc
Abhishek Kumar Tiwary <amannitr@rumba.seas.upenn.edu>
2018-08-16 16:40:17 -0400

    estimates computed correctly

--- plotC.m
+++ plotC.m
@@ -1,12 +1,12 @@
 function [] = plotC( Cset, Rset, scale )
     Colors = ['g', 'b', 'k' , 'm', 'c', 'r'];
-
+    labels = ['1', '2', '3', '4', '5', '6'];
     figure;
     hold on; grid on
 
     for i = 1 : size(Cset,2)
         % Show the ith camera
-        plotCamera('Location',Cset{i}*scale,'Orientation',Rset{i},'Opacity',0, 'Color', Colors(i));
+        plotCamera('Location',Cset{i}*scale,'Orientation',Rset{i},'Opacity',0, 'Color', Colors(i),'Label',labels(i));
     end
 
     view(360,0);
--- plotCam.m
+++ plotCam.m
@@ -1,7 +1,7 @@
 CSet = cell(1,6);
 RSet = cell(1,6);
 for i = 1 : 6
-   H = H3{i};
+   H = inv(H3{i});
    CSet{i} = H(1:3,4);
    RSet{i} = H(1:3,1:3)';
 end
